513c6873c1853fd86a7a5ea82581a2ea3385b17f
nostcoper <carlosserrato200475@gmail.com>
2025-05-03 02:13:55 -0500

    shooting system

--- Assets/Scrpts/BoatController.cs
+++ Assets/Scrpts/BoatController.cs
@@ -2,19 +2,35 @@ using UnityEngine;
 
 public class BoatController : MonoBehaviour
 {
-    public float fuerza = 500f;
+    public float turnSpeed;
+    public float acelerateSpeed;
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.angularDamping = 0.5f;
+        rb.maxAngularVelocity = 2f;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rb.AddTorque(0f, h * turnSpeed * Time.deltaTime, 0f);
+
+        if (Mathf.Abs(v) > 0.01f)
         {
-            rb.AddForce(Vector3.forward * fuerza);
+            rb.AddForce(transform.forward * v * acelerateSpeed * Time.deltaTime);
         }
-    }
+        else
+        {
+            Vector3 horizontalVelocity = rb.linearVelocity;
+            horizontalVelocity.y = 0; 
+
+            Vector3 brakeForce = -horizontalVelocity * 2f;
+            rb.AddForce(brakeForce, ForceMode.Acceleration);
+        }
+
+    }
 }
